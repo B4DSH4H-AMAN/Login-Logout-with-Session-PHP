5bb188b9f021a3293e814f0c7de938273f211218
Aman Upadhyay <76415079+B4DSH4H-AMAN@users.noreply.github.com>
2020-12-27 10:20:37 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'scott');
+   define('DB_PASSWORD', '0000');
+   define('DB_DATABASE', 'freelancing');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
---
+++ welcome.php
@@ -0,0 +1,10 @@
+<html">
+   <head>
+      <title>Welcome </title>
+   </head>
+   <body>
+      <h1>Welcome</h1>
+      <h2><a href = "logout.php">Sign Out</a></h2>
+      <h2><a href = "order.php">View Order</a></h2>
+   </body>
+</html>
